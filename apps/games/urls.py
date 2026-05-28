69da3ee06e925e6a29a2eabf1cab228958e0444d
yad3rii <yad3riiwork@gmail.com>
2026-05-28 22:26:18 +0300

    [games] сделал маршруты для страниц игр 28.05

--- apps/games/urls.py
+++ apps/games/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
+from . import views
+
 
 app_name = "games"
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.game_list_view, name="game_list"),
+    path("<slug:slug>/", views.game_detail_view, name="game_detail"),
+]
