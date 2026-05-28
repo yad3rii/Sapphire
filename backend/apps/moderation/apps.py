cb6eac080d1be83d55a63e5d4d76d942326eed18
yad3rii <yad3riiwork@gmail.com>
2026-05-28 22:55:59 +0300

    [cleanup] Удалил лишнюю папку 28.05

--- backend/apps/moderation/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ModerationConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.moderation"
-    label = "moderation"
