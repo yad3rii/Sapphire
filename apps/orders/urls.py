814e628a2bb35b0d09e5f711ca475c71aba97a33
yad3rii <yad3riiwork@gmail.com>
2026-05-29 14:58:40 +0300

    [orders] сделал маршруты для заказов 29.05

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 
+from . import views
+
 
 app_name = "orders"
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.order_list_view, name="order_list"),
+    path("sales/", views.my_sales_view, name="my_sales"),
+    path("create/<slug:listing_slug>/", views.create_order_view, name="create_order"),
+    path("<int:order_id>/", views.order_detail_view, name="order_detail"),
+    path("<int:order_id>/complete/", views.complete_order_view, name="complete_order"),
+    path("<int:order_id>/cancel/", views.cancel_order_view, name="cancel_order"),
+]
